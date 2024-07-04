91bc3cec21558c35767643f1d4173880fc4fe80a
NICOLAS RM <nicolas.rm540@gmail.com>
2024-07-04 01:52:23 -0600

    Configuraciones Finales Guard Permisos

--- src/modules/auth/guards/permissions-auth.guard.ts
+++ src/modules/auth/guards/permissions-auth.guard.ts
@@ -1,55 +1,66 @@
 import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../auth.service'; // Importación del servicio de autenticación
 
 @Injectable()
 export class PermissionsAuthGuard implements CanActivate {
     constructor(
-        private reflector: Reflector,
-        private authService: AuthService,
+        private reflector: Reflector, // Reflector para acceder a los metadatos de los decoradores
+        private authService: AuthService, // Servicio de autenticación para obtener permisos del usuario
     ) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        // Obtener los permisos requeridos del decorador
-        const requiredPermissions = this.reflector.get<string[]>('permissions', context.getHandler());
-        if (!requiredPermissions) {
-            // Si no hay permisos especificados, permitir acceso
-            return true;
-        }
-
-        const request = context.switchToHttp().getRequest();
-        const user = request.user;
-
-        // Verificar que el usuario esté presente en la solicitud
-        if (!user) {
-            throw new ForbiddenException('No se encontró usuario en la solicitud');
-        }
-
-        // Obtener los permisos del usuario
-        const userPermissions = await this.authService.getUserPermissions(user.id);
-        console.log('Permisos del usuario:', userPermissions);
-
-        // Verificar que el usuario tenga todos los permisos requeridos que se necesitan para acceder al recurso
-        const hasPermission = requiredPermissions.every((permission) => {
-            const permissionEntity = userPermissions.find((p) => permission.includes(p.entity));
-
-            if (permissionEntity) {
-                // Verificar que permisos tiene el endpoint
-                if (permission.includes('read')) {
-                    return permissionEntity.canRead === true;
-                } else if (permission.includes('write')) {
-                    return permissionEntity.canWrite === true;
-                }
+        try {
+            // Obtener los permisos requeridos del decorador usando el Reflector
+            const requiredPermissions = this.reflector.get<string[]>('permissions', context.getHandler());
+            if (!requiredPermissions) {
+                // Si no hay permisos especificados, permitir acceso
+                return true;
             }
 
-            return false;
-        });
+            // Obtener la solicitud HTTP del contexto
+            const request = context.switchToHttp().getRequest();
+            const user = request.user; // Obtener el usuario de la solicitud
 
-        // Si el usuario no tiene todos los permisos requeridos, lanzar una excepción
-        if (!hasPermission) {
-            throw new ForbiddenException('No tienes permiso para acceder a este recurso');
+            // Verificar que el usuario esté presente en la solicitud
+            if (!user) {
+                throw new ForbiddenException('No se encontró usuario en la solicitud');
+            }
+
+            // Obtener los permisos del usuario desde el servicio de autenticación
+            const userPermissions = await this.authService.getUserPermissions(user.id);
+
+            // Verificar que el usuario tenga todos los permisos requeridos
+            const hasPermission = requiredPermissions.every((permission) => {
+                const permissionEntity = userPermissions.find((p) => permission.includes(p.entity));
+
+                if (permissionEntity) {
+                    // Verificar permisos según el tipo (lectura o escritura)
+                    if (permission.includes('read')) {
+                        return permissionEntity.canRead === true;
+                    } else if (permission.includes('write')) {
+                        return permissionEntity.canWrite === true;
+                    }
+                }
+
+                return false; // El usuario no tiene el permiso requerido
+            });
+
+            // Si el usuario no tiene todos los permisos requeridos, lanzar una excepción
+            if (!hasPermission) {
+                throw new ForbiddenException('No tienes permiso para acceder a este recurso');
+            }
+
+            return true; // Permitir acceso si el usuario tiene todos los permisos requeridos
+        } catch (error) {
+            // Manejo de excepciones
+            if (error instanceof ForbiddenException) {
+                throw error; // Lanzar excepción ForbiddenException si ocurre
+            }
+
+            // Otras excepciones pueden manejarse aquí, como errores de base de datos
+
+            throw new ForbiddenException('Error de permisos'); // Lanzar una excepción genérica para otros errores
         }
-
-        return hasPermission;
     }
 }
